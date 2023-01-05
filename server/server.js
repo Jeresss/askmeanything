3e4cbfcf158f5224fee495fdb05af4aa1737c505
Cryptohefe <73742699+Cryptohefe@users.noreply.github.com>
2023-01-05 17:09:32 +0200

    finalll

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import { Configuration, OpenAIApi } from 'openai';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -11,19 +11,20 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'Hello from CodeX',
+        message: 'Hello from CodeX!'
     })
 })
 
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
+
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
@@ -44,4 +45,4 @@ app.post('/', async (req, res) => {
     }
 })
 
-//app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
